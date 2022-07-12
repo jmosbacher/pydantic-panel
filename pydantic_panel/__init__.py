fbbec62eb4906f714032a20151300dcc161b3ae5
Yossi Mosbacher <joe.mosbacher>
2022-07-12 21:49:41 +0300

    Bump version: 0.1.1 → 0.1.2

--- pydantic_panel/__init__.py
+++ pydantic_panel/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Yossi Mosbacher"""
 __email__ = "joe.mosbacher@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .dispatchers import json_serializable, get_widget
 from .widgets import (
